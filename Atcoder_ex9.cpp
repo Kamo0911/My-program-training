0ddcd433f73a7ad6ff9b94876a1c0da0b7d8e9bf
Kamo <akaminekaede@icloud.com>
2021-02-22 09:34:56 +0900

    複合代入演算子を使おう

---
+++ Atcoder_ex9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  x++;
+  cout << x << endl;
+
+  x*=(a+b);
+  cout << x << endl;
+  
+  x*=x;
+  cout << x << endl;
+  
+  x--;
+  cout << x << endl;
+}
