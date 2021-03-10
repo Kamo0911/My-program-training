c06498df828675c562464bbf8bc6f65627573a81
Kamo <akaminekaede@icloud.com>
2021-03-10 23:38:04 +0900

    電卓を作ろう２

---
+++ C++/Atcoder_ex11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N,A;
+  cin >> N >> A;
+	for(int i=0;i<N;i++){
+		string op;
+		int B;
+		cin >> op >> B;
+		if(op == "+"){
+			A+=B;
+		}else if(op == "-"){
+			A-=B;
+		}else if(op == "*"){
+			A*=B;
+		}else if(op == "/"){
+			A/=B;
+		}else{
+			cout << "error" << endl;
+			break;
+		}
+		
+		cout << i+1 << ":" << A << endl;
+	}
+}
