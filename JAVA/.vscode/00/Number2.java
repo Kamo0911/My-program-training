06c96d1896f538f277a9fa681742e7416e2a28d2
Kamo <akaminekaede@icloud.com>
2021-04-04 09:49:39 +0900

    配列の要素数を出力

---
+++ JAVA/.vscode/00/Number2.java
@@ -0,0 +1,13 @@
+class Number2 {
+    public static void main(String[] args){
+        int[][] a ={
+            {10,20,30},
+            {40,50},
+            {60,70,80,90}
+        };
+        System.out.println("一次元配列の様素数" + a.length);
+        System.out.println("a[0]の要素数" + a[0].length);
+        System.out.println("a[1]の要素数" + a[1].length);
+        System.out.println("a[2]の要素数" + a[2].length);
+    }
+}
