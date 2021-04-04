81e50b4ad466b7b603a64f4d3bb275cd4299d29f
Kamo <akaminekaede@icloud.com>
2021-04-04 09:48:50 +0900

    配列の出力

---
+++ JAVA/.vscode/00/Number.java
@@ -0,0 +1,18 @@
+class Number {
+    public static void main(String[] args){
+        int[] a = {1,2,3,4};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+
+        int[][] b = {
+            {10,20},
+            {30,40},
+            {50,60}
+        };
+        System.out.println("\nb[0][0] =" + b[0][0] + "b[0][1] =" + b[0][1]);
+        System.out.println("b[1][0] =" + b[1][0] + "b[1][1] =" + b[1][1]);
+        System.out.println("b[2][0] =" + b[2][0] + "b[2][1] =" + b[2][1]);
+    }
+}
