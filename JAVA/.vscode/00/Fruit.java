3bc555a0a7ce29248c3fe1d641e89d4e9548c94b
Kamo <akaminekaede@icloud.com>
2021-04-04 09:47:47 +0900

    Fruit

---
+++ JAVA/.vscode/00/Fruit.java
@@ -0,0 +1,9 @@
+class Fruit {
+    public static void main(String[] args){
+        String apple = "‚è‚ñ‚²";
+        int a = 3;
+
+        String fruit = apple + a + "‚±";
+        System.out.println(fruit);
+    }
+}
