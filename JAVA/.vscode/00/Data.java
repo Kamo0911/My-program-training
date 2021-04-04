be031fb88d0931633a50c3aa3cc14d03ea367515
Kamo <akaminekaede@icloud.com>
2021-04-04 09:55:16 +0900

    数値型2

---
+++ JAVA/.vscode/00/Data.java
@@ -0,0 +1,19 @@
+class Data {
+    public static void main(String[] args){
+        int a;
+        int b;
+        a = 2;
+        b = 3;
+        a = b;
+        byte c = 127;
+        short d = 10000;
+        float e = 1.2f;
+        double f = 0.34;
+
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }    
+}
